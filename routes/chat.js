31765253640d3b5e84469723d274aa68f4d1e08b
Lewis Tse <lewis.tse@airpoint.com.hk>
2018-05-17 14:51:03 +0800

    https support

--- routes/chat.js
+++ routes/chat.js
@@ -2,9 +2,23 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+//var server = require('http').createServer(app);
+
+var fs = require('fs');
+var key = fs.readFileSync('routes/encryption/private.key');
+var cert = fs.readFileSync( 'routes/encryption/mydomain.crt' );
+
+var options = {
+key: key,
+cert: cert
+//ca: ca
+};
+
+var server = require('https').createServer(options,app);
+
+var io = require('socket.io')(server,{secure: true});
 var Chat = require('../models/Chat.js');
+
 
 var userSocketID = new Array();
 var userSocketIDAndUsername = new Array();
@@ -12,6 +26,8 @@ var adminSocketID = new Array();
 var username = new Array();
 var atou = {};
 var utoa = {};
+
+
 
 var userSocketIDOperatorChannel  = {};
 var operatorSocketIDOperatorChannel = {};
@@ -88,11 +104,13 @@ io.on('connection', function (socket) {
     } else if (userid == 'operatorSessionUser'){
       userSocketIDOperatorChannel = socket.id;
     } else {
+        console.log('Lu log userid is '+ userid);   //userid is tel number
       userSocketIDAndUsername.push(userid + ' (' + socket.id + ')');
       userSocketID.push(socket.id);
       username.push(userid);
       for(var i in adminSocketID){
-        io.to(adminSocketID[i]).emit('users',{users:userSocketIDAndUsername});
+        //io.to(adminSocketID[i]).emit('users',{users:userSocketIDAndUsername});
+          io.to(adminSocketID[i]).emit('users',{users:userSocketIDAndUsername},socket.id);
       }
     }
     socket.userid = userid;
--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,22 +3,59 @@ import { ChatService } from '../chat.service';
 import * as io from 'socket.io-client';
 import * as $ from 'jquery';
 
+let connectedCustomers = {};   // Lu
+let currentTab;
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
+
 export class AdminComponent implements OnInit {
 
-  socket = io('http://192.168.0.102:3638');
+  //socket = io('https://192.168.0.102:3638');
+    
+   // socket = io('192.168.0.102:3638',{secure: true});
+    socket = io('https://192.168.0.102:3638',{secure: true});
 
-  constructor() { }
+  constructor() {
+      
+     
+        
+        
+//        let createNewCustomerTab = function(customerId) {
+//          var newChatElements: {customerId, tab, window};
+//          newChatElements.customerId = customerId;
+//          // A tab displaying the customer id
+//          newChatElements.tab = $('<li class="chat-tab">').text(customerId);
+//          // The chat log for this customer
+//          newChatElements.window = $('<ul class="chat-window">').hide();
+//
+//          var clickHandler = function() {
+//            setCurrentTab(newChatElements);
+//          };
+//          newChatElements.tab.click(clickHandler);
+//
+//          connectedCustomers[customerId] = newChatElements;
+//
+//          if(!currentTab) {
+//            console.log('Setting current tab');
+//            clickHandler();
+//          }
+//
+//          $('#chatTabs').append(newChatElements.tab);
+//          $('#chatWindows').append(newChatElements.window);
+//        } 
+  }
 
+    
+    
   ngOnInit() {
 
   this.socket.emit('user','admin');
 
-  this.socket.on('users', function(data){
+  /*this.socket.on('users', function(data){
   	console.log("inside socket.io users" +data);
   // alert(JSON.stringify(data));
   // $('#users').empty();
@@ -31,7 +68,9 @@ export class AdminComponent implements OnInit {
 	// console.log("inside socket.io users" +data);
   
 	});
-
+*/
+      
+            
   this.socket.on('chat', function(msg){
     $('#messages').append('<li>'+msg+'</li>');
   });
@@ -39,9 +78,67 @@ export class AdminComponent implements OnInit {
   this.socket.on('disconnect', function(msg){
     $('#messages').append('<li>'+msg+'</li>');
   });
+      
+       this.socket.on('users', function(arg1, arg2) {
+  // ...
+      
+           var newChatElements: any = {};
+    
+           
+   // var newChatElements:chatElements;         
+          
+     newChatElements.customerId = arg2;
+    // A tab displaying the customer id
+    newChatElements.tab = $('<li class="chat-tab">').text(arg2);
+    // The chat log for this customer
+    newChatElements.window = $('<ul class="chat-window">').hide();
+
+    var clickHandler = function() {
+        setCurrentTab(newChatElements);
+    };
+    newChatElements.tab.click(clickHandler);
+
+    //         connectedCustomers[arg2] = newChatElements;
+
+    if(!currentTab) {
+    console.log('Setting current tab');
+    clickHandler();
+    }
+
+    $('#chatTabs').append(newChatElements.tab);
+    $('#chatWindows').append(newChatElements.window);
+
+    $('#users').empty();
+    
+      for(var i in arg1){
+    $('#users').append('<li>'+arg1[i]+'</li>');
+      $('#users').append('<li>'+arg2+'</li>');
+    }
+      
+});
+      
+      
+           // Switch to a different tab
+        let setCurrentTab = function(target) {
+            // Do nothing if this is already the current tab
+            if(currentTab === target) return;
+            // Set the current tab
+            currentTab = target;
+            // Remove any other selected tab
+            $('li.chat-tab').removeClass('selected');
+            // Mark this tab as selected
+            target.tab.addClass('selected');
+            // Hide any other chat windows
+            $('.chat-window').hide();
+            // Show this chat window
+            target.window.show();
+        }
 
   }
 
+    
+    
+    
   SendForm(){
   	console.log("admin is sending a message");
   	this.socket.emit('chat message', $('#m').val());
@@ -55,5 +152,9 @@ export class AdminComponent implements OnInit {
     $('#u').val('');
     // return false;
   }
+
+  
+
+    
 
 }
