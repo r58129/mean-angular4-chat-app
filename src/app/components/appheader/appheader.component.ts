c90a4e3c1f033b5ebe99950eb140b997afb4b7d1
Lewis Tse <lewis.tse@airpoint.com.hk>
2018-05-29 15:46:46 +0800

    Hide more UI if user has not logged in

--- src/app/components/appheader/appheader.component.ts
+++ src/app/components/appheader/appheader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthserviceService } from '../../authservice.service';
 
 @Component({
   selector: 'app-appheader',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppheaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthserviceService) { }
 
   ngOnInit() {
   }
