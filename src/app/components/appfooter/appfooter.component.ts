9c256e7c61341ead2d219eab669abecc9cde3a55
Ben <shoulderapps@gmail.com>
2018-06-28 13:25:48 +0800

    hide login page menu, header and footer

--- src/app/components/appfooter/appfooter.component.ts
+++ src/app/components/appfooter/appfooter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthserviceService } from '../../authservice.service';
 
 @Component({
   selector: 'app-appfooter',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppfooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthserviceService) { }
 
   ngOnInit() {
   }
