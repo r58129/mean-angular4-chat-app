1fe6e41569143c9abbcb0528b7ec9f11f9407137
Ben <shoulderapps@gmail.com>
2018-12-14 16:57:36 +0800

    migrated to angular 6 with emoji added but it is not displayed properly

--- src/app/broadcast-detail/broadcast-detail.component.ts
+++ src/app/broadcast-detail/broadcast-detail.component.ts
@@ -34,6 +34,7 @@ export class BroadcastDetailComponent implements OnInit {
   filename: string;
   urlMessage: string;
   tinkerKey: string;
+  public openPopup: Function;
 
 
   broadcastDetail = { jobID:'', message: '', contactListCsvName:'', imagefile:'', imagefilename:'', notSendAck:'', prependContactName:'', jobStatus:''
@@ -442,31 +443,8 @@ export class BroadcastDetailComponent implements OnInit {
 
   } 
 
-  // disableBroadcastMode(){
-
-  //   // get admin sessionID
-  //   var sID=localStorage.getItem('res.data.sessionID');
-      
-  //   //construct form data
-  //   var disable = new FormData();
-  //   disable.append('sessionID', sID);
-
-  // 	this.chatService.disableBroadcast(disable).then((res) => {
-  		
-  // 		this.jobDetail = res;
-  		
-  // 		if (this.jobDetail.success =="true"){
-  // 			window.alert('Disabled broadcast mode! Other features is now resumed');
-  // 			console.log("disable broadcast");
-  // 		} else {
-  // 			console.log("disable broadcast failed!");
-  // 		}
-
-  // 	}, (err) => {
-		//   console.log(err);
-
-		// });	
-
-  // }
+  setPopupAction(fn: any) {
+    this.openPopup = fn;    
+  }
 
 }
